f42be1a0d7fea26c345ed22c1fbf20b329de1ab7
devarsh2395 <devarsh2395@gmail.com>
2023-01-23 09:23:39 -0500

    updated Code

---
+++ assets/script.js
@@ -0,0 +1,64 @@
+let questions = document.getElementById('questions');
+let checkAnswer = document.getElementById('check-answer');
+let playAgain = document.getElementById('play-again');
+let options = document.getElementById('options');
+let result = document.getElementById('result');
+let score = document.getElementById('score');
+let numberOfQuestions = document.getElementById('number-of-questions');
+
+
+
+
+let questionsToASk = [ 
+    
+    { 
+        'question' : 'What is the full-form of HTML?',
+        'a' : 'Hyperlinks and Text Markup Language',
+        'b' : 'Hyper Text Markup Language',
+        'c' : 'Home Tool Markup Language',
+        'd' : 'Hyper Markup Language',
+        'correct-answer' : 'b',
+},
+ {
+    'question' : 'What CSS stands for?',
+        'a' : 'Cascading style sheet',
+        'b' : 'Cascading sheet style',
+        'c' : 'Cyber Style sheet',
+        'd' : 'Cyber sheet style',
+        'correct-answer' : 'a',
+ },
+ {
+    'question' : 'What does h1 stands for in HTML?' ,
+        'a' : 'Header' ,
+        'b' : 'Heading 1' ,
+        'c' : 'Head 1' ,
+        'd' : 'None of the above' ,
+        'correct-answer' : 'b' ,
+
+ },
+ {
+    'question' : 'What does URL stand for?' ,
+        'a' : 'Unique registered Link' ,
+        'b' : 'Undefined Resource Locator' ,
+        'c' : 'Unique Resource Locator' ,
+        'd' : 'Uniform Resource Locator' ,
+        'correct-answer' : 'd' ,
+        
+ },
+ {
+    'question' : 'Which of the following is not a type of combinator? ' ,
+        'a' : '>' ,
+        'b' : '+' ,
+        'c' : '*' ,
+        'd' : '~' ,
+        'correct-answer' : 'c' ,
+        
+ }
+
+]
+
+
+
+
+
+
